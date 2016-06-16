54ff02e44858c599fe7e706d4b5e71bf56bcc145
gimmy700129 <sppmy@hotmail.com>
2016-06-16 13:34:07 +0800

    no message

---
+++ QMS/TNSBase/DBTool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+
+namespace TNSBase
+{
+    public static class DBTool
+    {
+        /// <summary>
+        /// DB連線
+        /// </summary>
+        /// <param name="dbConnType"></param>
+        /// <returns></returns>
+        public static string getDBConnectionString(DBConnType dbConnType)
+        {
+            return getDBConnectionString(dbConnType.ToString());
+        }
+
+        /// <summary>
+        /// DB連線
+        /// </summary>
+        /// <param name="strDBConn"></param>
+        /// <returns></returns>
+        public static string getDBConnectionString(string strDBConn)
+        {
+            string ConnectionString = string.Empty;
+            ConnectionStringSettings Setting = ConfigurationManager.ConnectionStrings[strDBConn];
+            if (Setting != null)
+                ConnectionString = Setting.ConnectionString;
+
+            return ConnectionString;
+        }
+    }
+}
---
+++ QMS/TNSBase/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TNSBase
+{
+    /// <summary>
+    /// 資料庫連線種類
+    /// </summary>
+    public enum DBConnType
+    {
+        ERP,
+        IMS,
+        OPERATION
+    }
+}
